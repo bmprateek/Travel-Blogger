719cf142edae7dca0dde6056554ddcc243719ba1
Aditya R Nayak <adityarnayak2001@users.noreply.github.com>
2022-07-15 23:09:42 +0530

    final changes

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,11 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleLg">TRAVEL BLOGGERS</span>
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
-        src="https://raw.githubusercontent.com/bmprateek/6th-SEM-LAB/main/Web%20Lab/1920x1080-px-architecture.jpg"
+        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
         alt=""
       />
     </div>
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,40 +1,26 @@
-import { Link } from "react-router-dom";
 import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post({img}) {
+export default function Post({ post }) {
+  const PF = "http://localhost:5000/images/";
   return (
     <div className="post">
-      <img
-        className="postImg"
-        src={img}
-        alt=""
-      />
+      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Music
-            </Link>
-          </span>
-          <span className="postCat">
-            <Link className="link" to="/posts?cat=Music">
-              Life
-            </Link>
-          </span>
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <span className="postTitle">
-          <Link to="/post/abc" className="link">
-          Magical Madeira
-          </Link>
-        </span>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
         <hr />
-        <span className="postDate">1 hour ago</span>
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
       </div>
-      <p className="postDesc">
-      Dramatic rugged mountains touching the clouds, hills covered in banana plantations, ocean as far as the eye can see, majestic waterfalls, hikes along waterways… Madeira offers the ideal island escape, a perfect part of Portugal to explore!
-
-If you are heading to Madeira, consider staying at the idilic Quinta Saraiva, shown in this video – you can get a discount on your booking via the code on my Portugal
-      </p>
+      <p className="postDesc">{post.desc}</p>
     </div>
   );
 }
